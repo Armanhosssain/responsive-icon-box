9bc422f9bb55ac7ee8b2d689f3cb3368d8d1316f
Arman hossain <armanh688@gmail.com>
2019-10-21 01:15:13 +0600

    responsive check box

---
+++ respoinsive-iconbox.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>responsive icon box </title>
+</head>
+<body>
+<!--start icon boxes -->
+<section id="icon-boxes" class="pt-2 pb-5 text-center text-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 mt-3">
+        <div class="card bg-success">
+          <div class="card-body">
+            <i class="fa fa-envelope"></i>
+            <h3>Lorem Ipsum</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quasi magni placeat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mt-3">
+        <div class="card bg-dark">
+          <div class="card-body">
+            <i class="fa fa-car"></i>
+            <h3>Lorem Ipsum</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quasi magni placeat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mt-3">
+        <div class="card bg-info">
+          <div class="card-body">
+            <i class="fa fa-play"></i>
+            <h3>Lorem Ipsum</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quasi magni placeat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 mt-3">
+        <div class="card bg-warning text-dark">
+          <div class="card-body">
+            <i class="fa fa-tree"></i>
+            <h3>Lorem Ipsum</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quasi magni placeat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mt-3">
+        <div class="card bg-info">
+          <div class="card-body">
+            <i class="fa fa-cart-plus"></i>
+            <h3>Lorem Ipsum</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quasi magni placeat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mt-3">
+        <div class="card bg-light text-dark">
+          <div class="card-body">
+            <i class="fa fa-youtube-square"></i>
+            <h3>Lorem Ipsum</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quasi magni placeat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end icon boxes -->
+<script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
